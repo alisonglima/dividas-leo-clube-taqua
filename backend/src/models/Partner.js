7b54aca0fbd0fa121294981fa6524afa67d4b2d2
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2022-03-15 11:55:35 -0300

    .

--- backend/src/models/Partner.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const PartnerSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-});
-
-module.exports = mongoose.model("Partner", PartnerSchema);
